4282a7317547fa3e3d7c03d9f43596475e593634
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2024-03-26 15:55:27 +0700

    update code

--- TrainingFPT/Controllers/CoursesController.cs
+++ TrainingFPT/Controllers/CoursesController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TrainingFPT.Helpers;
+using TrainingFPT.Models;
+using TrainingFPT.Models.Queries;
 
 namespace TrainingFPT.Controllers
 {
@@ -7,13 +11,92 @@ namespace TrainingFPT.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            CoursesViewModel course = new CoursesViewModel();
+            course.CourseDetailList = new List<CourseDetail>();
+            var dataCourses = new CourseQuery().GetAllDataCourses();
+            foreach (var data in dataCourses)
+            {
+                course.CourseDetailList.Add(new CourseDetail
+                {
+                    Id = data.Id,
+                    NameCourse = data.NameCourse,
+                    CategoryId = data.CategoryId,
+                    Description = data.Description,
+                    Status = data.Status,
+                    StartDate = data.StartDate,
+                    EndDate = data.EndDate,
+                    ViewImageCouser = data.ViewImageCouser,
+                    viewCategoryName = data.viewCategoryName
+                });
+            }
+            return View(course);
         }
 
         [HttpGet]
         public IActionResult Add()
         {
-            return View();
+            CourseDetail course = new CourseDetail();
+            List<SelectListItem> items = new List<SelectListItem>();
+            var dataCategories = new CategoryQuery().GetAllCategories(null, null);
+            foreach (var category in dataCategories)
+            {
+                items.Add(new SelectListItem
+                {
+                    Value = category.Id.ToString(),
+                    Text = category.Name
+                });
+            }
+            ViewBag.Categories = items;
+            return View(course);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(CourseDetail course, IFormFile Image)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //return Ok(course);
+                    string imageCourse = UploadFileHelper.UploadFile(Image);
+                    int idCourse = new CourseQuery().InsetDataCourse(
+                        course.NameCourse,
+                        course.CategoryId,
+                        course.Description,
+                        course.StartDate,
+                        course.EndDate,
+                        course.Status,
+                        imageCourse
+                    );
+                    if (idCourse > 0)
+                    {
+                        TempData["saveStatus"] = true;
+                    }
+                    else
+                    {
+                        TempData["saveStatus"] = false;
+                    }
+                    return RedirectToAction(nameof(CoursesController.Index), "Courses");
+                }
+                catch (Exception ex)
+                {
+                    // neu co loi
+                    return Ok(ex.Message);
+                }
+            }
+            List<SelectListItem> items = new List<SelectListItem>();
+            var dataCategories = new CategoryQuery().GetAllCategories(null, null);
+            foreach (var category in dataCategories)
+            {
+                items.Add(new SelectListItem
+                {
+                    Value = category.Id.ToString(),
+                    Text = category.Name
+                });
+            }
+            ViewBag.Categories = items;
+            return View(course);
         }
     }
 }
---
+++ TrainingFPT/Models/Queries/CourseQuery.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TrainingFPT.Models.Queries
+{
+    public class CourseQuery
+    {
+
+        public List<CourseDetail> GetAllDataCourses()
+        {
+            List<CourseDetail> courses = new List<CourseDetail>();
+            using (SqlConnection connection = Database.GetSqlConnection())
+            {
+                string sql = "SELECT [co].*, [ca].[Name] FROM [Courses] AS [co] INNER JOIN [Categories] AS [ca] ON [co].[CategoryId] = [ca].[Id] WHERE [co].[DeletedAt] IS NULL";
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        CourseDetail detail = new CourseDetail();
+                        detail.Id = Convert.ToInt32(reader["Id"]);
+                        detail.NameCourse = reader["NameCourse"].ToString();
+                        detail.Description = reader["Description"].ToString();
+                        detail.CategoryId = Convert.ToInt32(reader["CategoryId"]);
+                        detail.StartDate = Convert.ToDateTime(reader["StartDate"]);
+                        detail.EndDate = Convert.ToDateTime(reader["EndDate"]);
+                        detail.ViewImageCouser = reader["Image"].ToString();
+                        detail.Status = reader["Status"].ToString();
+                        detail.viewCategoryName = reader["Name"].ToString();
+                        courses.Add(detail);
+                    }
+                }
+                connection.Close();
+            }
+            return courses;
+        }
+
+        public int InsetDataCourse(
+            string nameCourse,
+            int categoryId,
+            string? description,
+            DateTime startDate,
+            DateTime? endDate,
+            string status,
+            string imageCourse
+        )
+        {
+            string valEndate = DBNull.Value.ToString();
+            if (endDate != null)
+            {
+                valEndate = endDate.Value.ToString();
+            }
+
+            int idCourse = 0;
+            using (SqlConnection connection = Database.GetSqlConnection())
+            {
+                string sqlQuery = "INSERT INTO [Courses]([CategoryId], [NameCourse], [Description], [Image], [Status], [StartDate], [EndDate], [CreatedAt]) VALUES(@CategoryId, @NameCourse, @Description, @Image, @Status, @StartDate, @EndDate, @CreatedAt) SELECT SCOPE_IDENTITY()";
+                // SELECT SCOPE_IDENTITY() : lay ra ID vua moi them.
+                connection.Open();
+                SqlCommand cmd = new SqlCommand( sqlQuery, connection );
+                cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+                cmd.Parameters.AddWithValue("@NameCourse", nameCourse);
+                cmd.Parameters.AddWithValue("@Description", description ?? DBNull.Value.ToString());
+                cmd.Parameters.AddWithValue("@Image", imageCourse);
+                cmd.Parameters.AddWithValue("@Status", status);
+                cmd.Parameters.AddWithValue("@StartDate", startDate);
+                cmd.Parameters.AddWithValue("@EndDate", valEndate);
+                cmd.Parameters.AddWithValue("@CreatedAt", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                idCourse = Convert.ToInt32(cmd.ExecuteScalar());
+                connection.Close();
+            }
+            return idCourse;
+        }
+    }
+}
